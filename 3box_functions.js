65f2c2a503cd2e23710fa5b0c1e91da08eb4f323
michal <m.a.p.zaleski@protonmail.com>
2020-07-23 12:59:49 +0200

    Update 3box_functions.js and package.json

--- 3box_functions.js
+++ 3box_functions.js
@@ -1,2 +1,3 @@
-const ipfsClient = require('ipfs-http-client');
-const ipfs = ipfsClient('http://localhost:5001'); // (the default in Node.js)
+//const ipfsClient = require('ipfs-http-client');
+//const ipfs = ipfsClient('http://localhost:5001'); // (the default in Node.js)
+const Box = require('3box')
