21657de60eec4f8601295520e2e32002950acdc4
Michał <michal@pop-os>
2020-09-04 15:54:35 +0200

    Update package-lock.json and package.json

--- 3box_functions.js
+++ 3box_functions.js
@@ -1,12 +1,15 @@
-
+//import Authereum from 'authereum'
+const {Authereum} = require('authereum')
 const Box = require('3box')
+// const authereum = new Authereum('kovan')
+const authereum = new Authereum('kovan')
+authereum.login()
 
-async function box1(){
+async function fbox1(){
   const profile = await Box.getProfile('0x268F8c402fAF167De657ADaC732cE737eAbac1aC')
   console.log(profile)
+  document.getElementById('div_3').innerHTML = profile.description
+
 }
 
-box1()
-
-// comment #1
-// comment #2
+fbox1()
