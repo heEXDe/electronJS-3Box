a2ca0c1850cc2a3a82339313ae73e7372b9c9661
Michał <michal@pop-os>
2020-09-04 11:35:19 +0200

    Update 3box_functions.js

--- 3box_functions.js
+++ 3box_functions.js
@@ -1,3 +1,9 @@
-//const ipfsClient = require('ipfs-http-client');
-//const ipfs = ipfsClient('http://localhost:5001'); // (the default in Node.js)
+
 const Box = require('3box')
+
+async function box1(){
+  const profile = await Box.getProfile('0x860714FFc0A1f8719986d9e072b69aF91682527a')
+  console.log(profile)
+}
+
+box1()
