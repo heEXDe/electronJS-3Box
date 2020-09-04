8e2f824cb022e7e56a4d8f995d882e2b60376069
Michał <michal@pop-os>
2020-09-04 13:14:04 +0200

    Update 3box_functions.js

--- 3box_functions.js
+++ 3box_functions.js
@@ -2,7 +2,7 @@
 const Box = require('3box')
 
 async function box1(){
-  const profile = await Box.getProfile('0x860714FFc0A1f8719986d9e072b69aF91682527a')
+  const profile = await Box.getProfile('0x268F8c402fAF167De657ADaC732cE737eAbac1aC')
   console.log(profile)
 }
 
